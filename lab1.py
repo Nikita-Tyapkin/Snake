37350efb8466b1568994cfbc7523b35f5372ab2a
Nikita-Tyapkin <nikita@red-forest.ru>
2021-11-12 19:51:57 +0300

    Hi

---
+++ lab1.py
@@ -0,0 +1,32 @@
+import turtle
+
+def S():
+    turtle.shape('turtle')
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+
+
+
+
+def kubik():
+    turtle.shape('arrow')
+    turtle.forward(300)
+    turtle.left(90)
+    turtle.forward(300)
+    turtle.left(90)
+    turtle.forward(300)
+    turtle.left(90)
+    turtle.forward(300)
+
+
+kubik()
+turtle.mainloop()
+
+
